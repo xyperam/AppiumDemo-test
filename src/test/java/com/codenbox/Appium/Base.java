71967f94b5f4da21ae62b739c093fc1c1377af9a
Ilham Imani Nur Alam <ilhamimaninuralam@gmail.com>
2024-05-17 01:17:48 +0700

    set wifi passed

--- src/test/java/com/codenbox/Appium/Base.java
+++ src/test/java/com/codenbox/Appium/Base.java
@@ -4,24 +4,36 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import com.google.common.collect.ImmutableMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public class Base {
-    public AndroidDriver driver;
-    public AppiumDriverLocalService service;
-
-    // Run Appium server automatically
-    @BeforeTest
-    public void configureAppium() {
+	public AndroidDriver driver;
+	public AppiumDriverLocalService service;
+	
+	
+	@BeforeClass
+    public void configureAppium() throws MalformedURLException {
+		Map<String , String> env = new HashMap<String , String>(System.getenv());
+		env.put("ANDROID_HOME", "C:\\Users\\ASUS\\AppData\\Local\\Android\\Sdk");
+		env.put("JAVA_HOME", "C:\\Program Files\\Java\\jdk-11.0.16.1");
+		
         service = new AppiumServiceBuilder()
                 .withAppiumJS(new File("C:\\Users\\ASUS\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
                 .withIPAddress("127.0.0.1")
@@ -30,27 +42,27 @@ public class Base {
                 .build();
         
         service.start();
-    }
-
-    @Test
-    public void startDriver() throws MalformedURLException {
-        // Create capabilities
+        
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Automation");
         options.setApp("D:\\code\\appium\\Appium\\src\\resources\\ApiDemos-Debug.Apk");
-
+        
         // Create object for Android driver
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        // Perform any actions or tests here
-        driver.quit();
     }
 
-    @AfterTest
-    public void tearDown() {
-        if (service != null) {
-            service.stop();
-        }
-    }
+ 
+        // Create capabilities
+   
+
+     
+     
+     @AfterClass
+     public void tearDown() {
+    		
+ 		driver.quit();
+ 		service.stop();
+     }
+
 }
