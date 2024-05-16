96fb22137fda2edb857432a2873846457509bbd9
Ilham Imani Nur Alam <ilhamimaninuralam@gmail.com>
2024-05-16 21:28:31 +0700

    setup server appium automatically

--- src/test/java/com/codenbox/Appium/Base.java
+++ src/test/java/com/codenbox/Appium/Base.java
@@ -1,25 +1,56 @@
 package com.codenbox.Appium;
+
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-public class Base {
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 
-	@Test
-	public void configureAppium() throws MalformedURLException{
-		//create capablities
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName("Automation");
-		options.setApp("D:\\code\\appium\\Appium\\src\\resources\\ApiDemos-Debug.Apk");
-		//create object for android driver
-		//AndroidDriver driver = new AndroidDriver(new URL( "http://192.168.160.155:4723"), options);
-		AndroidDriver driver = new AndroidDriver(new URL( "http://192.168.160.155:4723"), options);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(13));
-		driver.quit();
-		
-	}
+public class Base {
+    public AndroidDriver driver;
+    public AppiumDriverLocalService service;
+
+    // Run Appium server automatically
+    @BeforeTest
+    public void configureAppium() {
+        service = new AppiumServiceBuilder()
+                .withAppiumJS(new File("C:\\Users\\ASUS\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
+                .withIPAddress("127.0.0.1")
+                .usingPort(4723)
+                .withTimeout(Duration.ofSeconds(300))
+                .build();
+        
+        service.start();
+    }
+
+    @Test
+    public void startDriver() throws MalformedURLException {
+        // Create capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("Automation");
+        options.setApp("D:\\code\\appium\\Appium\\src\\resources\\ApiDemos-Debug.Apk");
+
+        // Create object for Android driver
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Perform any actions or tests here
+        driver.quit();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (service != null) {
+            service.stop();
+        }
+    }
 }
